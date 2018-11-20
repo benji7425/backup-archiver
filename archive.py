836e4db5ca58328868dc68a5a88aebacdf7d3fa7
benji7425 <benji7425@outlook.com>
2018-11-20 16:56:04 +0000

    Update logging messages for clarity

--- archive.py
+++ archive.py
@@ -30,14 +30,15 @@ root_dir = config["root_dir"]
 manifest_name = "backup.yml"
 paths = []
 
-logging.info("Beginning directory search in root {}".format(root_dir))
+logging.debug("=== BEGIN ===")
+logging.info("Beginning directory search in root '{}'".format(root_dir))
 for root, dirs, files in walk(root_dir):
     for dir in dirs:
         if dir.startswith("."):
             dirs.remove(dir)
 
     if (manifest_name in files):
-        logging.debug("Found manifest in {}".format(root))
+        logging.debug("Found manifest in '{}'".format(root))
         
         manifest_path = join(root, manifest_name)
         patterns = yaml.load(open(manifest_path, "r"))
@@ -55,20 +56,20 @@ for root, dirs, files in walk(root_dir):
 
         paths += include_paths
 
-        logging.debug("Including paths {}".format(include_paths))
+        logging.debug("Resolved include paths {}".format(include_paths))
 
         for path in include_paths:
             rel_path = relpath(path, root)
             if rel_path in dirs:
                 dirs.remove(rel_path)
-                logging.debug("Ignoring dir {} as included for backup already".format(dir))
+                logging.debug("Ignoring subdirs in '{}' as it's a backup dir itself".format(dir))
         
 logging.info("Finished scanning directories")
-logging.debug("All include paths: {}".format(paths))
+logging.debug("Found the following paths for backup: {}".format(paths))
 logging.info("Checking directories for modifications since last backup")
 
 for path in paths:
-    logging.debug("Checking result {} for updates".format(path))
+    logging.debug("Checking result '{}' for updates".format(path))
 
     full_path = join(root_dir, path)
     m_time = getmtime(full_path)  # get the last modified time
@@ -93,7 +94,12 @@ for path in paths:
         logging.debug(out)
         if err is not None:
             logging.warning(err)
+    else:
+        logging.debug("No updates found for '{}'".format(path))
 
+logging.debug("Writing updated modifed time data back to file")
 # write back the data file
 with open(config["data_file"], "w") as file:
     json.dump(data, file)
+
+logging.debug("=== DONE ===")
