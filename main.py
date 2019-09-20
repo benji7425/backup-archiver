e6514d6b162e1c744459b2d51b089976a05353ad
benji7425 <benji7425@pm.me>
2019-09-20 23:36:13 +0100

    Refactor various areas for clarity and simplicity

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ from os import walk
 from os.path import (dirname, exists, getmtime, isfile, join, normpath,
                      realpath, relpath)
 from subprocess import PIPE, Popen
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 import yaml
 
 
-def find_all_backup_paths(root_dir: str, manifest_name: str):
+def find_all_backup_paths(root_search_dir_abs: str, manifest_file_name: str) -> List[str]:
     """Find all the backup paths specified by any manifest file found anywhere within the root directory"""
 
-    logging.info("Beginning directory scan in root '{}'".format(root_dir))
-    results: List[str] = []
+    logging.info(f"Beginning directory scan in root '{root_search_dir_abs}'")
+    results_abs: List[str] = []
 
     # Iteratively walk the file tree
-    for current_dir, dirs, files in walk(root_dir):
+    for current_dir_abs, dirs, files in walk(root_search_dir_abs):
 
         # Remove any directories beginning with a .
         for dir in dirs:
@@ -26,86 +26,96 @@ def find_all_backup_paths(root_dir: str, manifest_name: str):
                 dirs.remove(dir)
 
         # Parse manifest file to get all the include paths
-        if (manifest_name in files):
-            include_paths = get_include_paths(current_dir, manifest_name)
+        if (manifest_file_name in files):
+            include_paths_abs = get_include_paths_abs(
+                current_dir_abs, manifest_file_name)
 
-            if include_paths is None:
+            if include_paths_abs is None:
                 continue
 
-            results += include_paths
+            results_abs += include_paths_abs
 
-            # I don't remember what the purpose of this line is but I think it is important
-            current_dir = relpath(current_dir, root_dir)
+            # Remove a path from the "to-process" list if it's going to be backed up itself
+            for path_abs in include_paths_abs:
+                path_rel = relpath(path_abs, current_dir_abs)
+                if path_rel in dirs:
+                    dirs.remove(path_rel)
+                    logging.debug(
+                        f"Ignoring subdirs in '{path_abs}' as it's a backup dir itself")
 
-        # Remove a path from the "to-process" list if it's going to be backed up itself
-        for path in results:
-            rel_path = relpath(join(root_dir, path),
-                               join(root_dir, current_dir))
-            if rel_path in dirs:
-                dirs.remove(rel_path)
-                logging.debug("Ignoring subdirs in '{}' as it's a backup dir itself"
-                              .format(join(root_dir, path)))
+    logging.info(f"Finished directory scan in root {root_search_dir_abs}")
 
-    logging.info("Finished directory scan in root {}".format(root_dir))
-
-    return results
+    return results_abs
 
 
-def get_include_paths(current_dir: str, manifest_name: str):
+def get_include_paths_abs(current_dir_abs: str, manifest_file_name: str) -> Optional[List[str]]:
     """Parse all the paths to ultimately include from a manifest file"""
 
     # Load the manifest file
-    manifest_path = join(current_dir, manifest_name)
-    logging.debug("Parsing manifest at '{}'".format(manifest_path))
-    patterns = yaml.load(open(manifest_path, "r"))
+    manifest_path = join(current_dir_abs, manifest_file_name)
+    logging.debug(f"Parsing manifest at '{manifest_path}'")
+    patterns = yaml.load(open(manifest_path, "r"), Loader=yaml.FullLoader)
 
     if patterns is None or "include" not in patterns:
         return None
 
     # Define functions for parsing glob paths
-    def get_normpath(path): return normpath(join(root_dir, current_dir, path))
+    def get_normpath_abs(path_rel) -> str:
+        """Get the absolute normalised path for a given path relative to the current dir"""
+        return normpath(join(current_dir_abs, path_rel))
 
-    def get_glob_paths(key): return [normpath(glob_path)
-                                     for glob_paths in [glob.glob(get_normpath(pattern)) for pattern in patterns[key]]
-                                     for glob_path in glob_paths]
+    def get_matching_paths_for_entry_abs(key) -> List[List[str]]:
+        """Get all of the matching paths for each entry in the given key in the current patterns dictionary"""
+        return [glob.glob(get_normpath_abs(pattern)) for pattern in patterns[key]]
+
+    def get_matching_paths_abs(key) -> List[str]:
+        """Get a flattened list of all the matching paths for a given key in the current patterns dictionary"""
+        return [normpath(matched_path) for matching_paths in get_matching_paths_for_entry_abs(key) for matched_path in matching_paths]
 
     # Parse all exclude paths
-    exclude_paths = get_glob_paths(
-        "exclude") if "exclude" in patterns else[]
+    exclude_paths_abs = \
+        get_matching_paths_abs("exclude") \
+        if "exclude" in patterns \
+        else []
+
+    # Parse all include paths and remove any that begin with a full stop
+    include_paths_unfiltered_abs = [matched_path for matched_path in get_matching_paths_abs("include")
+                                    if not re.search(r"\.\w+$", matched_path)]
 
     # Parse all include paths and remove any that match exclude paths
-    include_paths = [relpath(get_normpath(path), root_dir)
-                     for path in get_glob_paths("include")
-                     if (not re.search(r"\.\w+$", path) and path not in exclude_paths)]
+    include_paths_filtered_abs = [path for path in include_paths_unfiltered_abs
+                                  if path not in exclude_paths_abs]
 
-    logging.debug("Resolved include paths {}".format(include_paths))
+    logging.debug(f"Resolved include paths {include_paths_filtered_abs}")
 
-    return include_paths
+    return include_paths_filtered_abs
 
 
-def update_archives(paths: List[str], archive_directory: str, archive_command: str):
+def update_archives(root_search_dir_abs: str, paths_abs: List[str], archive_directory: str, archive_command: str) -> None:
     """Update all the archive files for the each of the given paths"""
 
     # Iterate all the given paths
-    for path in paths:
-        logging.debug("Checking result '{}' for updates".format(path))
+    for source_path_abs in paths_abs:
+        logging.debug(f"Checking result '{source_path_abs}' for updates")
 
         # Fetch last modified times for the origin path and archive file path
-        original_path = join(root_dir, path)
-        original_m_time = getmtime(original_path)  # Get the last modified time
-        archive_path = join(archive_directory, path)
-        archive_m_time = getmtime(
-            archive_path + ".7z") if isfile(archive_path + ".7z") else - 1
+        source_path_rel = relpath(source_path_abs, root_search_dir_abs)
+        source_modified_time = get_source_last_modified_time(source_path_abs)
+        archive_path_abs = join(archive_directory, source_path_rel)
+        archive_modified_time = \
+            getmtime(archive_path_abs + ".7z") \
+            if isfile(archive_path_abs + ".7z") \
+            else -1
 
         # Update the archive only if the origin has been modified since the archive was last written
-        if(original_m_time > archive_m_time):
-            logging.debug("Updating archive for path {}".format(path))
+        if(source_modified_time > archive_modified_time):
+            logging.debug(f"Updating archive for path {source_path_abs}")
 
             # Archive with 7zip
             process = Popen(
                 archive_command.format(
-                    archive=normpath(archive_path),
-                    directory=normpath(original_path)),
+                    archive=normpath(archive_path_abs),
+                    directory=normpath(source_path_abs)),
                 stdin=PIPE,
                 stdout=PIPE,
                 shell=True)
@@ -116,7 +126,37 @@ def update_archives(paths: List[str], archive_directory: str, archive_command: s
             if err is not None:
                 logging.warning(err)
         else:
-            logging.debug("No updates found for '{}'".format(path))
+            logging.debug(f"No updates found for '{source_path_abs}'")
+
+
+def get_source_last_modified_time(path_abs: str) -> float:
+    return getmtime(path_abs)
+
+
+def process_configuration(configuration: Dict[str, str]):
+    # Load configuration values
+    configuration_name = configuration["name"]
+    root_search_dir_abs = configuration["root_search_dir"]
+    manifest_file_name = configuration["manifest_file_name"]
+    archive_directory_abs = configuration["archive_directory"]
+    archive_command_unformatted = configuration["archive_command"]
+
+    logging.info(f"Begin processing configuration '{configuration_name}'")
+
+    # Parse and load all the backup paths for this configuration
+    paths_abs = find_all_backup_paths(root_search_dir_abs, manifest_file_name)
+
+    logging.debug(f"Checking for modification to following paths: {paths_abs}")
+
+    # Update the archives for all the paths in this configuration
+    update_archives(
+        root_search_dir_abs,
+        paths_abs,
+        archive_directory_abs,
+        archive_command_unformatted)
+
+    logging.info(f"Finished checking for modification to paths in {root_search_dir_abs}")
+    logging.info(f"Done processing configuration '{configuration_name}'")
 
 
 if __name__ == "__main__":
@@ -136,33 +176,12 @@ if __name__ == "__main__":
     with open("config.json", "r") as config_file:
         config = json.load(config_file)
 
-    configurations = config["configurations"]
+    configurations: List[Dict[str, str]] = config["configurations"]
 
     logging.debug("=== BEGIN ===")
 
     # Iterate over configurations
     for configuration in configurations:
-
-        logging.info("Begin processing configuration '{}'".format(
-            configuration["name"]))
-
-        # Load configuration values
-        root_dir = configuration["root_search_dir"]
-        manifest_name = configuration["manifest_file_name"]
-
-        # Parse and load all the backup paths for this configuration
-        paths = find_all_backup_paths(root_dir, manifest_name)
-
-        logging.debug("Checking for modification to following paths: {}"
-                      .format(paths))
-
-        # Update the archives for all the paths in this configuration
-        update_archives(
-            paths, configuration["archive_directory"], configuration["archive_command"])
-
-        logging.info("Finished checking for modification to paths in {}"
-                     .format(root_dir))
-        logging.info("Done processing configuration '{}'"
-                     .format(configuration["name"]))
+        process_configuration(configuration)
 
     logging.debug("=== DONE ===")
